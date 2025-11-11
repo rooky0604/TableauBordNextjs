2411df4fbadaf977495c5b35c065a3f3cee9cdf9
rooky0604 <rooky.info@gmail.com>
2025-11-11 14:37:24 +0100

    ajout d'une page

---
+++ src/app/(porfil)/page.tsx
@@ -0,0 +1,3 @@
+export default function PofilPage() {
+  return <section></section>;
+}
--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -1,3 +1,13 @@
+import TicketForm from "./TicketForm";
+
 export default function TicketPage() {
-  return <section></section>;
+  return (
+    <section>
+      <h1>Bienvenue sur la page de ticket</h1>
+
+      <p>
+        J'aimerai voir comment cela s'affiche sur le site avec des balises html
+      </p>
+    </section>
+  );
 }
