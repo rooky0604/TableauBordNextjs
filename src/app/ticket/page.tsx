e9d1a52960508001569b65053048433c18e21b62
rooky0604 <rooky.info@gmail.com>
2025-11-12 13:31:10 +0100

    ajoute componant ticket à la page

--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -4,6 +4,7 @@ export default function TicketPage() {
   return (
     <section>
       <h1>Bienvenue sur la page de ticket</h1>
+      <TicketForm />
 
       <p>
         J'aimerai voir comment cela s'affiche sur le site avec des balises html
