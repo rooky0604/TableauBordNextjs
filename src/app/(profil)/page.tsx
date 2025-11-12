bf3f49cf32b71a888c6f816f1b4acae0a5e93cea
rooky0604 <rooky.info@gmail.com>
2025-11-12 12:38:44 +0100

    ajout de la page profil au norme

---
+++ src/app/(profil)/page.tsx
@@ -0,0 +1,9 @@
+export default function TicketPage() {
+  return (
+    <section>
+      <h1>Bienvenue sur la page de profil</h1>
+
+      <p>Vérification si cela est bien dans la BAR NAV</p>
+    </section>
+  );
+}
--- src/app/(profil)/profil/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PofilPage() {
-  return <section></section>;
-}
